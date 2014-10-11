a24dc0bdf46a3d1b39da2ba245e58cb449930688
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-11 07:32:01 +0000

    [change] change of route of url

--- app/routes.php
+++ app/routes.php
@@ -16,7 +16,7 @@ Route::get('/login', 'BeLoginController@showLogin');
 Route::post('/admin/login', 'BeLoginController@doLogin');
 
 Route::group(array('before' => 'auth'), function() {
-	Route::get('/admin/dashboard', 'BeUserController@dashboard');
+	Route::get('/admin/dashboard', 'BeLoginController@dashboard');
 	Route::get('/admin/logout', 'BeLoginController@doLogout');
 	Route::get('/admin/users', 'BeUserController@listUser');
 	Route::any('/admin/create', 'BeUserController@createUser');
