dd17df19606eab5d532230109654f225fe06606c
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-15 05:53:15 +0000

    [fixed]
    Page Module:
    
    -Title of page should be unique
    - Should change status to incon

--- app/views/backend/modules/user/list.blade.php
+++ app/views/backend/modules/user/list.blade.php
@@ -60,16 +60,16 @@
                   				<td align="center">
                   					<a href='{{URL::to("admin/status")}}/{{$user->status}}/{{$user->id}}'>
                   						@if($user->status == 1)
-                  							<i class="btn btn-xs btn-success">Active</i>
+                  							<span class="icon-ok success"></span>
                   						@else
-                  							<i class="btn btn-xs btn-danger">In Active</i>
+                  							<span class="icon-remove danger"></span>
                   						@endif
                   					
                   					</a>
                   				</td>
                   				<td align="center">
-	                  				<a href="{{URL::to('admin/edit')}}/{{$user->id}}" ><i class="icon-edit"></i></a>
-	                  				<a  href="{{URL::to('admin/delete')}}/{{$user->id}}" onclick="return confirm('Are you sure you want to delete this item?');"><i class='icon-trash'></i></a>
+	                  				<a title="Edit" href="{{URL::to('admin/edit')}}/{{$user->id}}" ><i class="icon-edit success"></i></a>
+	                  				<a title="Delete" href="{{URL::to('admin/delete')}}/{{$user->id}}" onclick="return confirm('Are you sure you want to delete this item?');"><i class='icon-trash danger'></i></a>
 	                  			</td>
                   			</tr>
                   			<?php $i++;?>
