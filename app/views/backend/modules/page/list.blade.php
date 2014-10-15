9d2e491aba85f522a8fb54742489df66d68ae018
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-15 05:55:38 +0000

    [fixed]
    fixing icon color in user/list and page/list

--- app/views/backend/modules/page/list.blade.php
+++ app/views/backend/modules/page/list.blade.php
@@ -60,7 +60,7 @@
                   					</a>
                   				</td>
                   				<td align="center">
-	                  				<a title="Edit" href="{{URL::to('admin/edit_page')}}/{{$page->id}}" ><i class="icon-edit success"></i></a>
+	                  				<a title="Edit" href="{{URL::to('admin/edit_page')}}/{{$page->id}}" ><i class="icon-edit primary"></i></a>
 	                  				<a title="Delete" href="{{URL::to('admin/delete_page')}}/{{$page->id}}" onclick="return confirm('Are you sure you want to delete this item?');"><i class='icon-trash danger'></i></a>
 	                  			</td>
                   			</tr>
