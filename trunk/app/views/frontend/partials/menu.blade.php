bae2ebd89fa471cf5fc61b2c7424da2785faedd5
kimhim.hom@gmail.com <kimhim.hom@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-18 15:10:01 +0000

    [ADD] Template frontend custom integration

--- trunk/app/views/frontend/partials/menu.blade.php
+++ trunk/app/views/frontend/partials/menu.blade.php
@@ -1,1 +1,45 @@
-menu
+<!--=============Menu==========-->
+<div class="header-bottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse"
+						data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="category-tab">
+					<div class="mainmenu">
+						<ul class="nav nav-tabs navbar-nav collapse navbar-collapse">
+							<li><a href="#tshirt" data-toggle="tab">Home</a></li>
+							<li><a href="#blazers" data-toggle="tab">About us</a></li>
+							<li><a href="#sunglass" data-toggle="tab">Who wer are</a></li>
+							<li><a href="#kids" data-toggle="tab">Contact us</a></li>
+							<li><a href="#poloshirt" data-toggle="tab">Phone</a></li>
+							<li><a href="#sunglass" data-toggle="tab">Computer</a></li>
+							<li><a href="#kids" data-toggle="tab">Moto</a></li>
+							<li><a href="#poloshirt" data-toggle="tab">Car</a></li>
+							<li><a href="#sunglass" data-toggle="tab">House</a></li>
+							<li><a href="#kids" data-toggle="tab">Land</a></li>
+							<li><a href="#poloshirt" data-toggle="tab">Others</a></li>
+						</ul>
+					</div>
+				</div>
+				<!-- ========Start Breadcrumb here============ -->
+				<session id="breadcrumb">
+					<div class="row">
+						<div class="col-sm-12">
+							@yield('breadcrumb')
+						</div>
+					</div>
+				</session>
+				<!-- ========End Breadcrumb here============ -->
+			</div>
+		</div>
+	</div>
+</div>
+</header>
+<!--==============Closing header=========-->
--- trunk/app/views/frontend/partials/right.blade.php
+++ trunk/app/views/frontend/partials/right.blade.php
@@ -1,1 +1,112 @@
-right
+<!-- ========Right side========= -->
+<div class="col-sm-2">
+	<div class="right-sidebar">
+		<label class="btn-default get col-sm-12">Notification New Post(10)</label>
+		<div class="panel-group category-products" id="accordian">
+			<!--category-products-->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Laptop : Dell<strong class="number-display">&nbsp;300 $</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#"> Moto
+							: Scopy<strong class="number-display">&nbsp;2200 $</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#"> Phone
+							:Iphone5<strong class="number-display">&nbsp;850 $</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Desktop : Dell<strong class="number-display">&nbsp;150 $</strong>
+						</a>
+					</h4>
+				</div>
+			</div>
+		</div>
+		<!--/category-products-->
+
+		<!--=========Register seller============ -->
+		<label class="btn-default get col-sm-12">Product top post</label>
+		<div class="panel-group category-products" id="accordian">
+			<!--category-products-->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Electronic : <strong class="number-display">100</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							RealEstate : <strong class="number-display">100</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Clothes : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Jewelry : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Cometic : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#"> Food
+							: <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Furniture : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Vehicle : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordian" href="#">
+							Instrutment : <strong class="number-display">10</strong>
+						</a>
+					</h4>
+				</div>
+			</div>
+		</div>
+		<!--=========Register seller============ -->
+	</div>
+</div>
