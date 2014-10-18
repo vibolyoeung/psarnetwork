25b1f3bd95894abe2257c8d60acf6e4228ca0d45
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-18 11:39:35 +0000

    [Slideshow Module] create, delete, update, list and change status of slideshow

---
+++ trunk/app/controllers/BeSlideshowController.php
@@ -0,0 +1,233 @@
+<?php
+
+class BeSlideshowController extends BaseController {
+
+	private  $mod_slideshow;
+
+	public function __construct(){
+
+		$this->mod_slideshow = new Slideshow();
+	}
+
+	/**
+	 *
+	 * listSlideshow: this function using for listing all slideshow
+	 * @return array: all slideshows
+	 * @access public
+	 */
+	public function listSlideshow()
+	{
+		$result = $this->mod_slideshow->listingSlideshow();
+		return View::make('backend.modules.slideshow.list')->with('slideshows',$result);
+	}
+
+	/**
+	 *
+	 * createSlideshow: this function using for create new slideshow
+	 * @return true: if slideshow has been created
+	 * @access public
+	 */
+	public function createSlideshow(){
+		if(Input::has('btnSubmit')){
+			$rules = array(
+						'file' => 'required|mimes:jpeg,png,bmp,gif|image',
+						'title'=>'required',
+						'url'=>'required|url',
+						'createdDate'=>'required',
+						'ExpireDate'=>'required'
+					);
+
+			$validator = Validator::make(Input::all(), $rules);
+			if ($validator->passes()) {
+				$destinationPath = base_path() . '/public/upload/slideshow/';
+				self::generateFolderUpload($destinationPath);
+				$destinationPathThumb = $destinationPath.'thumb/';
+				$file = Input::file('file');
+				$fileName = $file->getClientOriginalName();
+				$fileName = self::generateFileName($destinationPath,$fileName);
+				$data = self::prepareDataBind('add', $fileName);
+				$advertiser = self::prepareDataAdvertiser();
+				$getLastId = $this->mod_slideshow->saveAddAdvertiser($advertiser);
+				$this->mod_slideshow->saveAddSlideshow($data, $getLastId);
+				$file->move($destinationPath, $fileName);
+				Image::make($destinationPath . $fileName)->resize(Config::get('constants.SLIDESHOW_RESIZE_WIDTH'), Config::get('constants.SLIDESHOW_RESIZE_HEGHT'))->save($destinationPathThumb . $fileName);
+				return Redirect::to('admin/slideshows')->with('SECCESS_MESSAGE','Slideshow has been added successfully');
+			}else{
+				return Redirect::to('admin/create-slideshow')->withInput()->withErrors($validator);
+			}
+		}
+
+		return View::make('backend.modules.slideshow.add');
+	}
+
+	/**
+	 *
+	 * editSlideshow: this function using for updating slideshow
+	 * @param id: id of slideshow
+	 * @return true: if the slideshow has been updated
+	 * @access public
+	 */
+	public function editSlideshow($id = null){
+		$id = (integer) $id;
+		if(Input::has('btnSubmit')){
+			$id = Input::get('hid');
+			$rules = array(
+						'title'=>'required',
+						'url'=>'required|url',
+						'createdDate'=>'required',
+						'ExpireDate'=>'required'
+					);
+			if(Input::hasfile('file')){
+				$rules['file'] = 'mimes:jpeg,png,bmp,gif|image';
+			}
+			$validator = Validator::make(Input::all(), $rules);
+			if ($validator->passes()) {
+				if(Input::hasfile('file')){
+					$destinationPath = base_path() . '/public/upload/slideshow/';
+					self::generateFolderUpload($destinationPath);
+					$destinationPathThumb = $destinationPath.'thumb/';
+					$file = Input::file('file');
+					$fileName = $file->getClientOriginalName();
+					$fileName = self::generateFileName($destinationPath,$fileName);
+					$data = self::prepareDataBind('edit', $fileName);
+					$this->mod_slideshow->saveEditSlideshow($id,$data, $param ='operation');
+					$file->move($destinationPath, $fileName);
+					Image::make($destinationPath . $fileName)
+						->resize(Config::get('constants.SLIDESHOW_RESIZE_WIDTH'), Config::get('constants.SLIDESHOW_RESIZE_HEGHT'))
+						->save($destinationPathThumb . $fileName);
+				}else{
+					$data = self::prepareDataBind('edit');
+					$this->mod_slideshow->saveEditSlideshow($id,$data, $param ='operation');
+				}
+				return Redirect::to('admin/slideshows')->with('SECCESS_MESSAGE','Slideshow has been added successfully');
+			}else{
+				return Redirect::to('admin/edit-slideshow/'.$id)->withInput()->withErrors($validator);
+			}
+		}
+		$result = $this->mod_slideshow->saveEditSlideshow($id);
+		return View::make('backend.modules.slideshow.edit')->with('slideshows', $result->data);
+	}
+
+	/**
+	 * deleteSlideshow: this function using for deleting an existing slideshow
+	 * @return true: if the slideshow has been deleted
+	 * @param id: the id of slideshow
+	 * @access public
+	 *
+	 */
+	public function deleteSlideshow($id=null){
+		$id = (integer)$id;
+		$result = $this->mod_slideshow->deleteSlideshow($id);
+		if(1 == $result->result){
+			return Redirect::to('admin/slideshows')->with('SECCESS_MESSAGE','Slideshow has been deleted successfully');
+		}else {
+			return Redirect::to('admin/slideshows')->with('ERROR_MESSAGE',$result->errorMsg);
+		}
+	}
+
+	/**
+	 *
+	 * isPublicSlideshow: this function using for disable and enable the slideshow
+	 * @param id: the id of slideshow
+	 * @param status: the status of slideshow
+	 * @return true: if the slideshow has been changed status
+	 * @access public
+	 */
+	public function isPublicSlideshow($id = null, $status = null){
+		$id = (integer) $id;
+		$status = (integer) $status;
+		$result = $this->mod_slideshow->isPublicSlideshow($id, $status);
+		if(1 == $result->result){
+			return Redirect::to('admin/slideshows')->with('SECCESS_MESSAGE','Slideshow has been changed status successfully');
+		}else {
+			return Redirect::to('admin/slideshows')->with('ERROR_MESSAGE',$result->errorMsg);
+		}
+	}
+
+	/**
+	 *
+	 * prepareDataBind: this function using for preparing data before inserting data into database
+	 * @param param: Edit | Add
+	 * @access private
+	 * @return array object
+	 */
+	private static function prepareDataBind($param, $fileName=null){
+
+		$data = array(
+				'title'=>trim(Input::get('title')),
+				'link_url'=>trim(Input::get('url')),
+				'created_date'=>trim(Input::get('createdDate')),
+				'expire_date'=>trim(Input::get('ExpireDate')),
+				'short_desc'=>trim(Input::get('short_desc')),
+				'description'=>trim(Input::get('desc'))
+		);
+
+		if($param == 'add'){
+			$data['image'] = $fileName;
+		}elseif ('edit'){
+			if(!empty($fileName)){
+				$data['image'] = $fileName;
+			}
+		}
+
+		return $data;
+	}
+
+	/**
+	 *
+	 * prepareDataAdvertiser: this function using for preparing advertiser
+	 * @return array:
+	 * @access private
+	 */
+	private static function prepareDataAdvertiser(){
+		$data = array();
+		if(Input::has('isAdvertiser')){
+					$data['name'] = trim(Input::get('name'));
+					$data['phone'] = trim(Input::get('phone'));
+					$data['email'] = trim(Input::get('email'));
+					$data['address'] = trim(Input::get('Address'));
+		}
+		return $data;
+	}
+
+	/**
+	 * Generation folder when uploading file doesnot exist
+	 * @return boolean
+	 * @access private
+	 * @method generateFolderUpload
+	 * @throws ErrorException
+	 */
+	private static function generateFolderUpload($destinationPath) {
+
+			$destinationPathThumb = $destinationPath.'/thumb/';
+			if (!file_exists($destinationPath)) {
+				mkdir($destinationPath, 0777, true);
+				if (!file_exists($destinationPathThumb)) {
+					mkdir($destinationPathThumb, 0777, true);
+				}
+			}
+
+	}
+
+	/**
+	 * Generation fileName when uploading file
+	 * @return filename by generation
+	 * @access public
+	 * @method generateFileName
+	 * @throws Exception
+	 */
+	public static function generateFileName($pathName, $fileName = null) {
+
+			$temp = explode(".", $fileName);
+			$fileName = end($temp);
+			$fileName = time(). '.' . $fileName;
+			if (file_exists($pathName . $fileName)) {
+				return generateFileName($pathName);
+			}
+
+		return $fileName;
+	}
+
+
+
+}
--- trunk/app/views/backend/modules/slideshow/edit.blade.php
+++ trunk/app/views/backend/modules/slideshow/edit.blade.php
@@ -1,38 +1,78 @@
 @extends('backend/layout')
 @section('title')
-    Create
+	Edit
 @endsection
 @section('breadcrumb')
-    <ul class="breadcrumb">
-        <li><a href="{{URL::to('admin/dashboard')}}">Dashboard</a></li>
-        <li><a href="{{URL::to('admin/pages')}}">Pages</a></li>
-        <li>Edit</li>
-    </ul>
+	<ul class="breadcrumb">
+	<li><a href="{{URL::to('admin/dashboard')}}">Dashboard</a></li>
+	<li><a href="{{URL::to('admin/slideshows')}}">Slideshow</a></li>
+	<li>Edit</li>
+	</ul>
 @endsection
 @section('content')
 <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-          <div class="panel panel-default">
-            <div class="panel-heading clearfix">
-              <i class="icon-calendar"></i>
-              <h3 class="panel-title">Edit</h3>
-            </div>
-            <div class="panel-body">
-             {{Form::open(array('url'=>'admin/edit_page'))}}
-             	<div class="form-group">
-                  <label>Title<span class="class-required">*</span></label>
-                 {{ Form::text('title',$pages->title, array('class' => 'form-control','placeholder'=>'Enter Title'))}}
-                 <span class="class-error">{{$errors->first('title')}}</span>
-                </div>
-                 {{Form::hidden('id',$pages->id)}}
-                <div class="form-group">
-                  <label>Short Description</label>
-                 {{ Form::textarea('desc',$pages->short_desc, array('class' => 'form-control','placeholder'=>'Enter description'))}}
-                </div>
-                {{Form::submit('Create', array('class' => 'btn btn-success','name'=>'btnSubmit'))}}
-              {{Form::close()}}
-            </div>
-          </div>
-        </div>
-      </div>
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="panel panel-default">
+			<div class="panel-heading clearfix">
+				<i class="icon-calendar"></i>
+				<h3 class="panel-title">Slideshows</h3>
+			</div>
+			<div class="panel-body">
+				{{Form::open(array('url'=>'admin/edit-slideshow','enctype'=>'multipart/form-data','file' => true))}}
+				<div class="form-group">
+					<label>Title<span class="class-required">*</span></label>
+					{{ Form::text('title',$slideshows->title, array('class' => 'form-control','placeholder'=>'Enter Title'))}}
+					<span class="class-error">{{$errors->first('title')}}</span>
+				</div>
+
+				<div class="form-group">
+					<label>Link URL<span class="class-required">*</span></label>
+					{{ Form::text('url',$slideshows->link_url, array('class' => 'form-control','placeholder'=>'Enter URL'))}}
+					<span class="class-error">{{$errors->first('url')}}</span>
+				</div>
+
+				<div class="form-group">
+					<label>Create Date <span class="class-required">*</span></label>
+					{{ Form::text('createdDate',$slideshows->created_date, array('class' => 'form-control datepicker','placeholder'=>'Enter Create Date'))}}
+					<span class="class-error">{{$errors->first('createdDate')}}</span>
+				</div>
+
+				<div class="form-group">
+					<label>Expire Date <span class="class-required">*</span></label>
+					{{ Form::text('ExpireDate',$slideshows->expire_date, array('class' => 'form-control datepicker','placeholder'=>'Enter Expire Date'))}}
+					<span class="class-error">{{$errors->first('ExpireDate')}}</span>
+				</div>
+
+				<div class="form-group">
+					<label>Picture<span class="class-required">*</span></label>
+					{{ Form::file('file',array('class' => 'form-control'))}}
+					<span class="class-error">{{$errors->first('file')}}</span>
+				</div>
+				<div class="form-group">
+						{{HTML::image("upload/slideshow/thumb/".$slideshows->image,$slideshows->title,array())}}
+				</div>
+
+				<div class="form-group">
+					<label>Short Description</label>
+					{{ Form::textarea('short_desc',$slideshows->short_desc, array('class' => 'form-control','placeholder'=>'Enter short description'))}}
+				</div>
+
+				<div class="form-group">
+					<label>Description</label>
+					{{ Form::textarea('desc',$slideshows->description, array('class' => 'form-control','placeholder'=>'Enter description'))}}
+				</div>
+				{{Form::hidden('hid',$slideshows->id)}}
+				{{Form::submit('Create', array('class' => 'btn btn-success','name'=>'btnSubmit'))}}
+				{{Form::close()}}
+			</div>
+		</div>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#isAdvertiserOption').hide();
+			isAdvertiserDisplay();
+		});
+	</script>
  @endsection
