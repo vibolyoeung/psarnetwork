ed29438436bfd2ffc8789dd31b8e60e58804a2d7
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-23 14:30:15 +0000

    Sample search category

---
+++ trunk/app/views/frontend/modules/search/index.blade.php
@@ -0,0 +1,14 @@
+@extends('frontend.layout')
+@section('title')
+	Search
+@endsection
+@section('breadcrumb')
+	<ol class="breadcrumb">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Library</a></li>
+		<li class="active">Data</li>
+	</ol>
+@endsection
+@section('content')
+ddd
+@endsection
