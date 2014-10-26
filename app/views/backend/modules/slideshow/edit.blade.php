806de78f584fc7ad73a903c184c204ea3f15596a
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-10-26 10:17:59 +0000

    [Fixed] put datepicker Slideshw module

--- app/views/backend/modules/slideshow/edit.blade.php
+++ app/views/backend/modules/slideshow/edit.blade.php
@@ -19,31 +19,31 @@
 			</div>
 			<div class="panel-body">
 				{{Form::open(array('url'=>'admin/edit-slideshow','enctype'=>'multipart/form-data','file' => true))}}
-				<div class="form-group">
+				<div class="form-group col-md-6 col-sm-12 col-xs-6">
 					<label>Title<span class="class-required">*</span></label>
 					{{ Form::text('title',$slideshows->title, array('class' => 'form-control','placeholder'=>'Enter Title'))}}
 					<span class="class-error">{{$errors->first('title')}}</span>
 				</div>
 
-				<div class="form-group">
+				<div class="form-group col-md-6 col-sm-12 col-xs-6">
 					<label>Link URL<span class="class-required">*</span></label>
 					{{ Form::text('url',$slideshows->link_url, array('class' => 'form-control','placeholder'=>'Enter URL'))}}
 					<span class="class-error">{{$errors->first('url')}}</span>
 				</div>
 
-				<div class="form-group">
+				<div class="form-group col-md-6 col-sm-12 col-xs-6">
 					<label>Create Date <span class="class-required">*</span></label>
 					{{ Form::text('createdDate',$slideshows->created_date, array('class' => 'form-control datepicker','placeholder'=>'Enter Create Date'))}}
 					<span class="class-error">{{$errors->first('createdDate')}}</span>
 				</div>
 
-				<div class="form-group">
+				<div class="form-group col-md-6 col-sm-12 col-xs-6">
 					<label>Expire Date <span class="class-required">*</span></label>
 					{{ Form::text('ExpireDate',$slideshows->expire_date, array('class' => 'form-control datepicker','placeholder'=>'Enter Expire Date'))}}
 					<span class="class-error">{{$errors->first('ExpireDate')}}</span>
 				</div>
 
-				<div class="form-group">
+				<div class="form-group col-md-6 col-sm-12 col-xs-6">
 					<label>Picture<span class="class-required">*</span></label>
 					{{ Form::file('file',array('class' => 'form-control'))}}
 					<span class="class-error">{{$errors->first('file')}}</span>
@@ -52,7 +52,7 @@
 						{{HTML::image("upload/slideshow/thumb/".$slideshows->image,$slideshows->title,array())}}
 				</div>
 
-				<div class="form-group">
+				<div class="form-group ">
 					<label>Short Description</label>
 					{{ Form::textarea('short_desc',$slideshows->short_desc, array('class' => 'form-control','placeholder'=>'Enter short description'))}}
 				</div>
@@ -62,7 +62,7 @@
 					{{ Form::textarea('desc',$slideshows->description, array('class' => 'form-control','placeholder'=>'Enter description'))}}
 				</div>
 				{{Form::hidden('hid',$slideshows->id)}}
-				{{Form::submit('Create', array('class' => 'btn btn-success','name'=>'btnSubmit'))}}
+				{{Form::submit('Update', array('class' => 'btn btn-success','name'=>'btnSubmit'))}}
 				{{Form::close()}}
 			</div>
 		</div>
