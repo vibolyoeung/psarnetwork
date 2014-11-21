038c3b795e5ebc8b664a67b89efcbf6b864acb63
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-11-21 16:51:20 +0000

    customization of the name functionality in backend

--- trunk/app/views/backend/partials/menu.blade.php
+++ trunk/app/views/backend/partials/menu.blade.php
@@ -9,28 +9,59 @@
 </div>
 <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
 	<ul class="nav navbar-nav">
-		<li class="active"><a href="{{URL::to('admin/dashboard')}}">Dashboard</a>
-		</li>
 		<li class="dropdown"><a href="#" class="dropdown-toggle"
-			data-toggle="dropdown">System<b class="caret"></b></a>
+			data-toggle="dropdown">User Management<b class="caret"></b></a>
 			<ul class="dropdown-menu">
-				<li><a href="{{URL::to('admin/users')}}">Users</a></li>
-			</ul></li>
+				<li><a href="{{URL::to('admin/users')}}">System User</a></li>
+				<li><a href="#">Client User</a></li>
+				<li><a href="#">User Role</a></li>
+				<li><a href="#">Client User Type</a></li>
+			</ul>
+		</li>
 
 		 <li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Catalog <b class="caret"></b></a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Product Management<b class="caret"></b></a>
 			<ul class="dropdown-menu">
-				<li><a href="./forms.html">Product</a></li>
+				<li><a href="#">Product</a></li>
 				<li><a href="{{URL::to('admin/categories')}}">Category</a></li>
 			</ul>
 		</li>
-		<li><a href="{{URL::to('admin/pages')}}">Page</a></li>
-		<li><a href="{{URL::to('admin/markets')}}">Market</a></li>
 		<li class="dropdown"><a href="#" class="dropdown-toggle"
-			data-toggle="dropdown">Media <b class="caret"></b></a>
+			data-toggle="dropdown">System Management <b class="caret"></b></a>
+			<ul class="dropdown-menu">
+			<li><a href="{{URL::to('admin/pages')}}">Page</a></li>
+			<li><a href="#">Setting</a></li>
+		</ul>
+		</li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle"
+			data-toggle="dropdown">Media Management <b class="caret"></b></a>
 			<ul class="dropdown-menu">
 				<li><a href="{{URL::to('admin/slideshows')}}">Slideshow</a></li>
 				<li><a href="{{URL::to('admin/advertisements')}}">Advertisement</a></li>
+				<li><a href="{{URL::to('admin/markets')}}">Super Market</a></li>
+			</ul>
+		</li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle"
+			data-toggle="dropdown">Report Management<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li><a href="#">User Report</a></li>
+				<li><a href="#">Product Report </a></li>
+				<li><a href="#">Banner Report</a></li>
+				<li><a href="#">Page Report</a></li>
+			</ul>
+		</li>
+		<li class="dropdown"><a href="#" class="dropdown-toggle"
+			data-toggle="dropdown">Message Management<b class="caret"></b></a>
+			<ul class="dropdown-menu">
+				<li ><a href="#">New User Register</a></li>
+				<li><a href="#">New User Expired </a></li>
+				<li><a href="#">Page Expired</a></li>
+				<li><a href="#">Page Updated</a></li>
+				<li><a href="#">Banner New Register</a></li>
+				<li><a href="#">Banner Expired</a></li>
+				<li><a href="#">Product PIONT To New</a></li>
+				<li><a href="#">Product PIONT TO update</a></li>
+				<li><a href="#">Product PIONT TO Expired </a></li>
 			</ul>
 		</li>
 	</ul>
