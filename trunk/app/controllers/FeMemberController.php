ca1b72d016c5ab265b0d3d045221a61b9e5b8ffe
info.neakporn@gmail.com <info.neakporn@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-11-25 17:42:28 +0000

    create register for choose user type /public/member/register/free and /public/member/register/enterprise

--- trunk/app/controllers/FeMemberController.php
+++ trunk/app/controllers/FeMemberController.php
@@ -7,9 +7,16 @@ class FeMemberController extends BaseController {
         return View::make('frontend.modules.member.index');
     }
 
-    public function register() {
-
-        return View::make('frontend.modules.member.register');
+    public function register($usertype = '') {
+        if (!empty($usertype)) {
+            if ($usertype == 'enterprise') {
+                return View::make('frontend.modules.member.enterprise');
+            } else {
+                return View::make('frontend.modules.member.freesell');
+            }
+        } else {
+            return View::make('frontend.modules.member.register');
+        }
     }
 
 }
