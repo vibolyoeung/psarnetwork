655eeb0eda1bf5b895774b9125d85bcd290a49c0
yoeung.vibol@gmail.com <yoeung.vibol@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-11-25 16:43:28 +0000

    [TASK] List and add advertisement

--- app/models/Advertisement.php
+++ app/models/Advertisement.php
@@ -1,76 +1,95 @@
 <?php
-
-class Advertisement extends Eloquent{
-
-	public  $timestamps = false;
+class Advertisement extends Eloquent {
+	public $timestamps = false;
 
 	/**
 	 *
+	 * listingAdvertisment: list all advertisments
+	 * @return array: get all advertisments
+	 * @access public
+	 * @throws Exception: can not list advertisment
+	 */
+	public function listingAdvertisment(){
+		$response = new stdClass();
+
+		$adv = Config::get ('constants.TABLE_NAME.ADVERTISEMENT');
+		$advPage = Config::get ('constants.TABLE_NAME.ADV_PAGE' );
+		$advPostion = Config::get ('constants.TABLE_NAME.ADV_POSITION');
+		try {
+			$result = DB::table($adv .' AS adv')
+			->select('adv.id', 'adv.title', 'adv.description', 'adv.link_url', 'adv.started_date', 'adv.expire_date', 'adv.image', 'adv.status', 'advp.name AS pageName', 'ps.name AS positionName')
+			->join($advPage .' AS advp', 'adv.adv_page_id','=', 'advp.id')
+			->join($advPostion . ' AS ps', 'adv.adv_position_id', '=', 'ps.id')
+			->orderBy('adv.id','desc')
+			->paginate(Config::get('constants.BACKEND_PAGINATION_AVERTISEMENT'));
+			$response->data = $result;
+			$response->result = 1;
+		}catch (\Exception $e){
+			$response->result = 0;
+			$response->errorMsg = $e->getMessage();
+		}
+		return  $response;
+	}
+
+	/**
+	 *
+	 *
 	 * saveAddAdvertisement:this function using for create new advertisement
-	 * @param getLastId: this last id of advertisement
+	 *
 	 * @param data: the data of advertisement
 	 * @return true: if the advertisement has been created
 	 * @access public
 	 * @throws Exception
 	 */
-	public function saveAddAdvertisement($data = array(),$getLastId){
+	public function saveAddAdvertisement($data = array()) {
+		$response = new stdClass ();
 
-		$response = new stdClass();
 		try {
-			if(!empty($getLastId->isEmpty)){
-				$data['advertiser_id'] = $getLastId->lastId;
-			}
-			DB::table(Config::get('constants.TABLE_NAME.ADVERTISEMENT'))->insertGetId($data);
-			$response->result =1;
-		}catch (\Exception $e){
+			DB::table ( Config::get ( 'constants.TABLE_NAME.ADVERTISEMENT' ) )->insertGetId ( $data );
+			$response->result = 1;
+		} catch ( \Exception $e ) {
 			$response->result = 0;
-			$response->errorMsg = $e->getMessage();
+			$response->errorMsg = $e->getMessage ();
 		}
+
 		return $response;
 	}
-
 	public function findAllAdvertisePages() {
-		$response = new stdClass();
+		$response = new stdClass ();
 		try {
-			$result = DB::table(Config::get('constants.TABLE_NAME.ADV_PAGE'))->get();
-			$advPageArr = array();
-			$advPageArr[0] = 'Choose Advertisement Page';
-			foreach($result as $advPage) {
-				$advPageArr[$advPage->id] = $advPage->name;
+			$result = DB::table ( Config::get ( 'constants.TABLE_NAME.ADV_PAGE' ) )->get ();
+			$advPageArr = array ();
+			$advPageArr [0] = 'Choose Advertisement Page';
+			foreach ( $result as $advPage ) {
+				$advPageArr [$advPage->id] = $advPage->name;
 			}
 			$response->data = $advPageArr;
 			$response->result = 1;
-		} catch (\Exception $e){
+		} catch ( \Exception $e ) {
 			$response->result = 0;
-			$response->errorMsg = $e->getMessage();
+			$response->errorMsg = $e->getMessage ();
 		}
 
 		return $response;
 	}
-
 	public function findPostionByPageAdsId($id) {
-		$response = new stdClass();
+		$response = new stdClass ();
 
-		$advPostion = Config::get('constants.TABLE_NAME.ADV_POSITION');
-		$advPagePostion = Config::get('constants.TABLE_NAME.ADV_PAGE_POSITION_MM');
+		$advPostion = Config::get ( 'constants.TABLE_NAME.ADV_POSITION' );
+		$advPagePostion = Config::get ( 'constants.TABLE_NAME.ADV_PAGE_POSITION_MM' );
 		try {
-			$result = DB::table($advPostion . ' AS p')
-					->join($advPagePostion . ' AS pp', 'p.id', '=', 'pp.adv_position_id')
-					->where('pp.adv_page_id', '=', $id)
-					->select('p.id', 'p.name')
-					->get();
-			$advPositionArr = array();
-			$advPositionArr[0] = 'Please choose position';
-			foreach($result as $position) {
-				$advPositionArr[$position->id] = $position->name;
+			$result = DB::table ( $advPostion . ' AS p' )->join ( $advPagePostion . ' AS pp', 'p.id', '=', 'pp.adv_position_id' )->where ( 'pp.adv_page_id', '=', $id )->select ( 'p.id', 'p.name' )->get ();
+			$advPositionArr = array ();
+			$advPositionArr [0] = 'Please choose position';
+			foreach ( $result as $position ) {
+				$advPositionArr [$position->id] = $position->name;
 			}
 			$response->data = $advPositionArr;
 			$response->result = 1;
-		} catch (\Exception $e) {
+		} catch ( \Exception $e ) {
 			$response->result = 0;
-			$response->errorMsg = $e->getMessage();
+			$response->errorMsg = $e->getMessage ();
 		}
 		return $response;
 	}
-
 }
