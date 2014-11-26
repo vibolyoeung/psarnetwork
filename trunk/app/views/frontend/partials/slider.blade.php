9cd93e6979a0e0ca7d0d921548209e89cddcae34
kimhim.hom@gmail.com <kimhim.hom@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-11-26 08:19:33 +0000

    [IMPROVEMENT] slideshow front final

--- trunk/app/views/frontend/partials/slider.blade.php
+++ trunk/app/views/frontend/partials/slider.blade.php
@@ -1,3 +1,4 @@
+use Symfony\Component\Translation\Tests\String;
 <section id="slider">
 	<!--slider-->
 	<div class="row">
@@ -5,45 +6,65 @@
 			<div id="slider-carousel" class="carousel slide" data-ride="carousel">
 				<ol class="carousel-indicators">
 				<?php $i=0;?>
-					@foreach($slideshows as $slideshow)
-						<?php 
-							$exp_date = $slideshow->expire_date;
-							$exp_date = str_replace('/', '-', $exp_date);
-							
-							$created_date = $slideshow->created_date;
-							$created_date =str_replace('/', '-', $created_date); 
-						?>
-						@if(strtotime($exp_date) >= strtotime($created_date))
-							<li data-target="#slider-carousel" data-slide-to="<?php echo $i;?>" class="bullet"></li>
-						@endif
+				@foreach($slideshows as $slideshow)
+				<?php 
+					if($slideshow->pro_id == ''){
+						$expire_date = $slideshow->sli_expire_date;
+						$expire_date =str_replace('/', '-', $expire_date);
+						if(strtotime(date("Y-m-d")) <= strtotime($expire_date)){ 
+							echo '<li data-target="#slider-carousel" data-slide-to="'.$i.'" class="bullet"></li>';
+						}
+					}else{
+						$expire_date = $slideshow->pro_expire_date;
+						$expire_date =str_replace('/', '-', $expire_date);
+						if(strtotime(date("Y-m-d")) <= strtotime($expire_date)){
+						 	echo '<li data-target="#slider-carousel" data-slide-to="'.$i.'" class="bullet"></li>';
+						}
+					}		
+				?>
+						
 						<?php $i++;?>
-					@endforeach
+				@endforeach
 				</ol>
 				<div class="carousel-inner">
 				
 				@foreach($slideshows as $slideshow)
 					<?php 
-						$exp_date = $slideshow->expire_date;
-						$exp_date = str_replace('/', '-', $exp_date);
-						
-						$created_date = $slideshow->created_date;
-						$created_date =str_replace('/', '-', $created_date); 
-					?>
-					@if(strtotime($exp_date) >= strtotime($created_date))
-						<div class="item">
-						<div class="col-sm-6">
-							<h3>
-								<span>{{$slideshow->title;}}</span>
-							</h3>
-							<p>{{$slideshow->short_desc;}}</p>
-							<a href='{{URL::to("product/product_detail")}}/{{$slideshow->id}}' class="btn btn-default get">View Details</a>
-						</div>
-						<div class="col-sm-6">
-							<img src="{{Config::get('app.url')}}/upload/slideshow/{{$slideshow->image;}}" class="girl img-responsive" alt="" /> 
-							<!-- <img src="{{Config::get('app.url')}}/frontend/images/home/pricing.png" class="pricing" alt="" /> -->
-						</div>
-					</div>
-					@endif
+						if($slideshow->pro_id == ''){
+							$exp_date = $slideshow->sli_expire_date;
+							$exp_date =str_replace('/', '-', $exp_date);
+							if(strtotime(date("Y-m-d")) <= strtotime($exp_date)){ ?>
+								<div class="item">
+									<div class="col-sm-12">
+										<a href='{{URL::to("product/product_detail")}}/{{$slideshow->id}}' target="_blank">
+											<img src="{{Config::get('app.url')}}/upload/slideshow/{{$slideshow->sli_image;}}" class="img-responsive" alt="" />
+										</a>
+									</div>
+								</div>
+						<?php
+							}
+						}
+						else{	
+							$exp_date = $slideshow->pro_expire_date;
+							$exp_date =str_replace('/', '-', $exp_date);
+							if(strtotime(date("Y-m-d")) <= strtotime($exp_date)){ ?>
+							<div class="item">
+								<div class="col-sm-6">
+									<h5>
+										{{$slideshow->sli_title;}}
+									</h5>
+									<p>{{$slideshow->sli_desc;}}</p>
+									<a href='{{URL::to("product/product_detail")}}/{{$slideshow->id}}' class="btn btn-default get">View Details</a>
+								</div>
+								<div class="col-sm-6">
+									<img src="{{Config::get('app.url')}}/upload/product/{{$slideshow->pro_image;}}" class="img-responsive" alt="" /> 
+									<!-- <img src="{{Config::get('app.url')}}/frontend/images/home/pricing.png" class="pricing" alt="" /> -->
+								</div>
+							</div>
+						<?php 
+							}
+						}
+						?>
 				@endforeach
 				</div>
 				<a href="#slider-carousel" class="left control-carousel hidden-xs"
