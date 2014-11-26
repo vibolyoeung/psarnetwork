7392a97795ccc76f663d7a73a638d002b9bb59a4
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-11-26 14:34:00 +0000

    Put permission access page and modify page on user module

--- trunk/app/views/backend/modules/user/user_profile.blade.php
+++ trunk/app/views/backend/modules/user/user_profile.blade.php
@@ -27,13 +27,20 @@
                 <p>{{Session::get('SECCESS_MESSAGE')}}</p>
               </div>
               @endif
+              @if(Session::has('ERROR_MODIFY_MESSAGE'))
+				<div class="alert alert-block alert-danger fade in">
+				<button data-dismiss="alert" class="close" type="button"
+					data-original-title="">x</button>
+				<p>{{Session::get('ERROR_MODIFY_MESSAGE')}}</p>
+				</div>
+				@endif
              {{Form::open(array('url'=>'admin/profile'))}}
              	<div class="form-group">
                   <label>Name</label>
                  {{ Form::text('name',$users->name, array('class' => 'form-control','placeholder'=>'Enter Name'))}}
                  <span class="class-error">{{$errors->first('name')}}</span>
                 </div>
-                
+
                 {{Form::submit('Save', array('class' => 'btn btn-success','name'=>'btnSubmit'))}}
               {{Form::close()}}
             </div>
