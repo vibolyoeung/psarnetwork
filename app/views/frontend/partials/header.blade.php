42d718043bd17cfdd5a3d991f46352d66819dfd8
kimhim.hom@gmail.com <kimhim.hom@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-12-23 16:19:36 +0000

    [IMPROVEMENT] product detail pop up

--- app/views/frontend/partials/header.blade.php
+++ app/views/frontend/partials/header.blade.php
@@ -13,6 +13,7 @@
         {{HTML::style('frontend/css/bootstrap.min.css')}}
         {{HTML::style('frontend/css/responsive.css')}}
         {{HTML::style('frontend/css/main.css')}}
+        {{HTML::style('frontend/css/layout.css')}}
         {{HTML::script('frontend/js/jquery.js')}}
         <!--[if lt IE 9]>
                 {{HTML::script('frontend/js/html5shiv.js')}}
