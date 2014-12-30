0d5e5d8ff719b3dcee10d9013d92a0a69dc2fe62
info.neakporn@gmail.com <info.neakporn@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2014-12-30 15:37:23 +0000

    socheat - fixed register page

--- trunk/app/controllers/FeMemberController.php
+++ trunk/app/controllers/FeMemberController.php
@@ -9,25 +9,26 @@ class FeMemberController extends BaseController {
         $this->mod_slideshow = new Slideshow();
     	$this->mod_category = new MCategory();
     	$this->mod_setting = new Setting();
+       
     }
 
-    public function index() {
 
-        $listCategories = self::getCategoriesHomePage();
-        return View::make('frontend.modules.member.index')
-                        ->with('slideshows', $listSlideshows->result)
-                        ->with('maincategories', $listCategories->result);
-    }
     public function index()
     {
-    	$limit = $this->mod_setting->getSlidshowNumber();
+         $limit = $this->mod_setting->getSlidshowNumber();
+    	$listSlideshows = self::getSlideshowToHomePage($limit->data->setting_value);
+    	$listCategories = self::getCategoriesHomePage();
+    	return View::make('frontend.modules.member.index')
+    	->with('slideshows', $listSlideshows->result)
+    	->with('maincategories', $listCategories->result);	
+    }
+    public function register($usertype = '') {
+         $limit = $this->mod_setting->getSlidshowNumber();
     	$listSlideshows = self::getSlideshowToHomePage($limit->data->setting_value);
     	$listCategories = self::getCategoriesHomePage();
     	return View::make('frontend.modules.member.index')
     	->with('slideshows', $listSlideshows->result)
     	->with('maincategories', $listCategories->result);
-    }
-    public function register($usertype = '') {
         if (!empty($usertype)) {
             if ($usertype == 'enterprise') {
                 return View::make('frontend.modules.member.enterprise');
@@ -49,9 +50,5 @@ class FeMemberController extends BaseController {
     	return $Category;
     }
 
-    public function getCategoriesHomePage() {
-        $Category = $this->mod_category->getMainCategories();
-        return $Category;
-    }
 
 }
