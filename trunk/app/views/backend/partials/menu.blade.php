5119d4af36b3e4c5d049e5341e8ffbb76ee835f7
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-01-08 16:00:57 +0000

    prepare menu backend

--- trunk/app/views/backend/partials/menu.blade.php
+++ trunk/app/views/backend/partials/menu.blade.php
@@ -13,7 +13,8 @@
 			data-toggle="dropdown">General Setting <b class="caret"></b></a>
 			<ul class="dropdown-menu">
 			<li><a href="{{URL::to('admin/pages')}}">Page</a></li>
-			<li><a href="{{URL::to('admin/setting-list')}}">Setting</a></li>
+			<li><a href="{{URL::to('admin/setting-list')}}">Front-end Setting</a></li>
+			<li><a href="{{URL::to('admin/setting-list')}}">Back-end Setting</a></li>
 		</ul>
 		<li class="dropdown"><a href="#" class="dropdown-toggle"
 			data-toggle="dropdown">User Management<b class="caret"></b></a>
@@ -37,7 +38,6 @@
 		<li class="dropdown"><a href="#" class="dropdown-toggle"
 			data-toggle="dropdown">Business Management <b class="caret"></b></a>
 			<ul class="dropdown-menu">
-				<li><a href="{{URL::to('admin/slideshows')}}">Slideshow</a></li>
 				<li><a href="{{URL::to('admin/advertisements')}}">Advertisement</a></li>
 				<li><a href="{{URL::to('admin/markets')}}">Supermarket</a></li>
 			</ul>
