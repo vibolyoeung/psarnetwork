eae92263645ec480b35591b157982548a3297465
info.neakporn@gmail.com <info.neakporn@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-01-28 10:32:20 +0000

    fixed layout for register page

--- trunk/app/views/frontend/nosidebar.blade.php
+++ trunk/app/views/frontend/nosidebar.blade.php
@@ -2,18 +2,47 @@
 	<div class="header-middle"><!--header-middle-->
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-6">
-					<div class="search_box">
-						<form class="navbar-form" role="search">
-						    <div class="form-group col-sm-12">
-						    	<div class="col-lg-12">
-						    		<button type="submit" class="btn btn-success pull-right">
-						    			<span class="glyphicon glyphicon-search"></span>
-						    		</button>
-						    		<input type="text" class="col-lg-11 pull-left search-box" placeholder="Search">
-						    	</div>
-						    </div>
-						</form>
+				<div class="col-lg-7" style="border:2px solid #F79646;padding:0;margin:0;">
+					<div class="col-lg-8 pull-right" style="padding:0;margin:0;">
+						<div class="col-lg-2 pull-right" style="padding:0;margin:0;">
+							<button type="submit" class="btn btn-warning pull-right col-lg-12" style="border-radius:0;">
+								<span class="glyphicon glyphicon-search"></span> Search
+							</button>
+						</div>
+						<div class="col-lg-10" style="padding:0;margin:0;">
+							<input type="text" class="form-control" placeholder="Search here" style="border-radius:0;border:none;border-left:1px solid #ddd;"/>
+						</div>
+					</div>
+					<div class="col-lg-4" style="margin:0;padding:0;">
+						<div class="col-lg-6 pull-right" style="margin:0;padding:0;">
+							<div class="btn-group col-lg-12" style="padding:0;margin:0;">
+							<button type="button" class="col-lg-12 btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="border-radius:0;border:none;">
+							    Location <span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Separated link</a></li>
+							  </ul>
+							</div>
+						</div>
+						
+						<div class="col-lg-6" style="margin:0;padding:0;">
+							<div class="btn-group col-lg-12 " style="margin:0;padding:0;">
+								  <button type="button" class="col-lg-12 btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false" style="border-radius:0;border:none;border-right:1px solid #ddd;">
+								    Type <span class="caret"></span>
+								  </button>
+								  <ul class="dropdown-menu" role="menu">
+								    <li><a href="#">Action</a></li>
+								    <li><a href="#">Another action</a></li>
+								    <li><a href="#">Something else here</a></li>
+								    <li class="divider"></li>
+								    <li><a href="#">Separated link</a></li>
+								  </ul>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
