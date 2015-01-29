ef675353a94d85eb9f51b01f1d3ab5a856a536d0
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-01-29 16:10:56 +0000

    [ADD] Create Layout for free page

---
+++ trunk/app/controllers/FeFreePageController.php
@@ -0,0 +1,12 @@
+<?php
+class FeFreePageController extends BaseController {
+
+	function __construct() {
+
+	}
+
+	public function freePage($name) {
+		return View::make('frontend.modules.freepage.free_page');
+	}
+
+}
