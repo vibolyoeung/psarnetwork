334a4b6a4707f87f67ae9b473be543131cc77041
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-02-21 15:33:07 +0000

    [IMPROVEMENT] account_role.sql

--- trunk/database/account_role.sql
+++ trunk/database/account_role.sql
@@ -1,16 +1,37 @@
 /*
 SQLyog Ultimate v10.00 Beta1
-MySQL - 5.6.12-log 
+MySQL - 5.6.12-log : Database - psarnetwork_db
 *********************************************************************
-*/
+*/
+
 /*!40101 SET NAMES utf8 */;
 
-create table `account_role` (
-	`rol_id` int ,
-	`rol_name` varchar ,
-	`acc_type_id` int 
-); 
-insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('1','Supplier','0');
-insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('2','Distributor','0');
-insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('3','Retialer','0');
-insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('4','Personal','0');
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`psarnetwork_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `psarnetwork_db`;
+
+/*Table structure for table `account_role` */
+
+DROP TABLE IF EXISTS `account_role`;
+
+CREATE TABLE `account_role` (
+  `rol_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rol_name` varchar(100) DEFAULT NULL,
+  `acc_type_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`rol_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `account_role` */
+
+insert  into `account_role`(`rol_id`,`rol_name`,`acc_type_id`) values (1,'Supplier',0),(2,'Distributor',0),(3,'Retialer',0),(4,'Personal',0);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
