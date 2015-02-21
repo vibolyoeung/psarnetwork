25972bd77b8ac8709e56763dc70bbbf8fb714319
info.neakporn@gmail.com <info.neakporn@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-02-21 13:05:56 +0000

    just add new table

---
+++ trunk/database/account_role.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.12-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `account_role` (
+	`rol_id` int ,
+	`rol_name` varchar ,
+	`acc_type_id` int 
+); 
+insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('1','Supplier','0');
+insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('2','Distributor','0');
+insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('3','Retialer','0');
+insert into `account_role` (`rol_id`, `rol_name`, `acc_type_id`) values('4','Personal','0');
