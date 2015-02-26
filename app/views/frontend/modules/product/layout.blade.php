5dc8245e682532970fb381b86ef151c1739e1e0f
doeunkoch@gmail.com <doeunkoch@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-02-26 17:13:15 +0000

    [TASK] Product list template

---
+++ app/views/frontend/modules/product/layout.blade.php
@@ -0,0 +1,20 @@
+@include('frontend.modules.product.partials.header')
+@include('frontend.modules.product.partials.menu')
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-2" style="padding:0;">
+				<div class="left-sidebar">
+					@yield('left')
+				</div>
+			</div>
+			@yield('content')
+			<div class="col-lg-1"  style="padding:0;">
+				<div class="left-sidebar">
+					@yield('right')
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+@include('frontend.modules.store.partials.footer');
