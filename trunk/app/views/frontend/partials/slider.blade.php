73adcdfb51c1a51478d3fbb2fb5bc475061e0d27
yoeung.vibol@gmail.com <yoeung.vibol@gmail.com@ba9b0173-4849-d331-354a-0c1e15a49968>
2015-02-28 07:53:21 +0000

    [FIXED] Problem with pro_id

--- trunk/app/views/frontend/partials/slider.blade.php
+++ trunk/app/views/frontend/partials/slider.blade.php
@@ -8,19 +8,11 @@
 					<?php $i=0;?>
 					@foreach($slideshows as $slideshow)
 					<?php
-						if($slideshow->pro_id == ''){
-							$expire_date = $slideshow->sli_expire_date;
-							$expire_date =str_replace('/', '-', $expire_date);
-							if(strtotime(date("Y-m-d")) <= strtotime($expire_date)){ 
-								echo '<li data-target="#slider-carousel" data-slide-to="'.$i.'" class="bullet"></li>';
-							}
-						}else{
-							$expire_date = $slideshow->pro_expire_date;
-							$expire_date =str_replace('/', '-', $expire_date);
-							if(strtotime(date("Y-m-d")) <= strtotime($expire_date)){
-							 	echo '<li data-target="#slider-carousel" data-slide-to="'.$i.'" class="bullet"></li>';
-							}
-						}		
+						$expire_date = $slideshow->end_date;
+						$expire_date =str_replace('/', '-', $expire_date);
+						if(strtotime(date("Y-m-d")) <= strtotime($expire_date)){
+							echo '<li data-target="#slider-carousel" data-slide-to="'.$i.'" class="bullet"></li>';
+						}
 						$i++;
 					?>
 					@endforeach
@@ -28,36 +20,36 @@
 					<div class="carousel-inner" style="border: 2px solid #ddd;padding:0;max-height: 200px;">
 					@foreach($slideshows as $slideshow)
 						<?php
-							if($slideshow->pro_id == ''){
-								$exp_date = $slideshow->sli_expire_date;
+							if($slideshow->type == 1){
+								$exp_date = $slideshow->end_date;
 								$exp_date =str_replace('/', '-', $exp_date);
 								if(strtotime(date("Y-m-d")) <= strtotime($exp_date)){ ?>
 									<div class="item">
 											<a href='{{URL::to("product/product_detail")}}/{{$slideshow->id}}' target="_blank">
-												<img src="{{Config::get('app.url')}}/upload/slideshow/{{$slideshow->sli_image;}}" class="" alt="" />
+												<img src="{{Config::get('app.url')}}/upload/advertisement/{{$slideshow->image;}}" class="" alt="" />
 											</a>
 									</div>
 							<?php
 								}
 							}
-							else{	
-								$exp_date = $slideshow->pro_expire_date;
+							else{
+								$exp_date = $slideshow->end_date;
 								$exp_date =str_replace('/', '-', $exp_date);
 								if(strtotime(date("d-m-Y")) <= strtotime($exp_date)){ ?>
 								<div class="item">
 									<div class="col-sm-6">
 										<h5>
-											{{$slideshow->sli_title;}}
+											{{$slideshow->title_en;}}
 										</h5>
-										<p>{{$slideshow->sli_desc;}}</p>
+										<p>{{$slideshow->description_en;}}</p>
 										<a href='{{URL::to("product/product_detail")}}/{{$slideshow->id}}' class="btn btn-default get">View Details</a>
 									</div>
 									<div class="col-sm-6">
-										<img src="{{Config::get('app.url')}}/upload/product/{{$slideshow->pro_image;}}" class="img-responsive" alt="" /> 
+										<img src="{{Config::get('app.url')}}/upload/advertisement/{{$slideshow->image;}}" class="img-responsive" alt="" />
 										<!-- <img src="{{Config::get('app.url')}}/frontend/images/home/pricing.png" class="pricing" alt="" /> -->
 									</div>
 								</div>
-							<?php 
+							<?php
 								}
 							}
 							?>
@@ -65,18 +57,29 @@
 					</div>
 					<a href="#slider-carousel" class="left control-carousel hidden-xs"
 						data-slide="prev"> <i class="fa fa-angle-left"></i>
-					</a> 
+					</a>
 					<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next"> <i
 						class="fa fa-angle-right"></i>
 					</a>
 				</div>
 			</div>
-			
+
 			<!-- -============Advertise top special======== -->
 			<div class="col-lg-4" style="border:2px solid #ddd;padding:5px;max-height: 200px;overflow:hidden;">
-				<a href="#" >
-					<img src="{{Config::get('app.url')}}frontend/images/top-advertise.jpg" alt="" title="" class="img-responsive"/>
-				</a> 
+				@foreach($advHorizontalTopLarges as $adv)
+				<?php
+					$exp_date = $adv->end_date;
+					$exp_date =str_replace('/', '-', $exp_date);
+					if(strtotime(date("d-m-Y")) <= strtotime($exp_date)){ ?>
+						<a href="{{$adv->link_url}}" >
+							<img
+								src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
+								class="img-responsive"
+								alt=""
+							/>
+						</a>
+				<?php } ?>
+				@endforeach
 			</div>
 		</div>
 	</div>
