5583336e5b98c1d121aed11d80157b011c3a37c9
Vibol Yoeung <yoeung.vibol@gmail.com>
2015-04-20 20:52:34 +0700

    [TASK] Horizontal middle advertisement

---
+++ app/views/frontend/partials/horizontal_center_adv.blade.php
@@ -0,0 +1,32 @@
+<!-- -============Advertise top special======== -->
+<?php $i = 0; ?>
+@foreach($advHorizontalLargeCenters as $adv)
+<?php
+	$exp_date = $adv->end_date;
+	$exp_date =str_replace('/', '-', $exp_date);
+	if(strtotime(date("d-m-Y")) <= strtotime($exp_date)){ ?>
+		<a href="{{$adv->link_url}}" >
+			<img
+				src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
+				class="img-responsive"
+				alt=""
+			/>
+		</a>
+<?php
+		$i ++;
+	}
+?>
+@endforeach
+<?php
+	if ($i === 0) {
+?>
+	<a href="#" >
+		<img
+			src="{{Config::get('app.url')}}frontend/images/default_ad_1135_85.jpg"
+			class="img-responsive"
+			alt=""
+		/>
+	</a>
+<?php
+	}
+?>
--- app/views/frontend/partials/products/latest.blade.php
+++ app/views/frontend/partials/products/latest.blade.php
@@ -332,20 +332,7 @@
 	</div>
 	<div class="col-lg-12">
 		<div class="col-lg-12 center-advertise">
-			@foreach($advHorizontalLargeCenters as $adv)
-			<?php
-				$exp_date = $adv->end_date;
-				$exp_date =str_replace('/', '-', $exp_date);
-				if(strtotime(date("d-m-Y")) <= strtotime($exp_date)){ ?>
-					<a href="{{$adv->link_url}}" >
-						<img
-							src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
-							class="img-responsive"
-							alt=""
-						/>
-					</a>
-			<?php } ?>
-			@endforeach
+			@include('frontend.partials.horizontal_center_adv')
 		</div>
 	</div>
 </div>
