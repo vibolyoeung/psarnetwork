e41d76f7c80d8590e799ad578d6f96707289a873
kimhim <kimhim.hom@gmail.com>
2015-04-28 22:03:54 +0700

    [BUGFIX] Fix product not category not found

--- app/views/frontend/modules/detail/index.blade.php
+++ app/views/frontend/modules/detail/index.blade.php
@@ -27,6 +27,9 @@ Categories
 			</div>
 		</div>
 		<div class="col-lg-12">
+		<?php 
+		if(count($productByCategory) > 0){
+		?>
 			@foreach($productByCategory as $product)
 				<div class="product_list_container">
 					<div class="media commnets">
@@ -49,111 +52,12 @@ Categories
 					</div>
 				</div>
 			@endforeach
+		<?php 
+			}else{
+				echo '<h3><center style="color:red;">Product not found!</center></h3>';
+			}
+		?>
 		</div>
-	</div>
-	<!-- Modal -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	  <div class="modal-dialog">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-	        <h4 class="modal-title" id="myModalLabel">Product details title</h4>
-	      </div>
-	      <div class="modal-body">
-	      		<div class="container">
-			        <div id="main_area">
-			                <!-- Slider -->
-			                <div class="row">
-			                    <div class="col-xs-12" id="slider">
-			                        <!-- Top part of the slider -->
-			                        <div class="row">
-			                            <div class="col-sm-8" id="carousel-bounding-box">
-			                                <div class="carousel slide" id="myCarousel">
-			                                    <!-- Carousel items -->
-			                                    <div class="carousel-inner">
-			                                        <div class="active item" data-slide-number="0">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			
-			                                        <div class="item" data-slide-number="1">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			
-			                                        <div class="item" data-slide-number="2">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			
-			                                        <div class="item" data-slide-number="3">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			
-			                                        <div class="item" data-slide-number="4">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			
-			                                        <div class="item" data-slide-number="5">
-			                                        <img src="{{Config::get('app.url')}}/frontend/images/home/blackview.jpg"></div>
-			                                    </div><!-- Carousel nav -->
-			                                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-			                                        <span class="glyphicon glyphicon-chevron-left"></span>                                       
-			                                    </a>
-			                                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-			                                        <span class="glyphicon glyphicon-chevron-right"></span>                                       
-			                                    </a>                                
-			                                    </div>
-			                            </div>
-			                            <div class="col-sm-4" id="carousel-text"></div>
-			                            <div id="slide-content" style="display: none;">
-			                                <div id="slide-content-0">
-			                                    <h2>
-			                                    	{{$product->title}}
-			                                    </h2>
-			                                    <p>
-			                                    	{{$product->description}}
-			                                    </p>
-			                                    <p class="sub-text">
-			                                    	{{$product->created_date}} - 
-			                                    	<a href="{{Config::get('app.url')}}/product/details/{{$product->id}}">Read more</a></p>
-			                                </div>
-			                            </div>
-			                        </div>
-			                    </div>
-			                </div><!--/Slider-->
-			
-			                <div class="row hidden-xs" id="slider-thumbs">
-			                	<div class="col-lg-8">
-			                		<!-- Bottom switcher of slider -->
-			                        <ul class="hide-bullets">
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-0"><img src="http://placehold.it/170x100&text=one"></a>
-			                            </li>
-			
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-1"><img src="http://placehold.it/170x100&text=two"></a>
-			                            </li>
-			
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-2"><img src="http://placehold.it/170x100&text=three"></a>
-			                            </li>
-			
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-3"><img src="http://placehold.it/170x100&text=four"></a>
-			                            </li>
-			
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-4"><img src="http://placehold.it/170x100&text=five"></a>
-			                            </li>
-			
-			                            <li class="col-sm-2">
-			                                <a class="thumbnail" id="carousel-selector-5"><img src="http://placehold.it/170x100&text=six"></a>
-			                            </li>
-			                        </ul> 
-			                	</div>                
-			                </div>
-			        </div>
-			</div>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-			<!--<button type="button" class="btn btn-primary">Save changes</button> -->
-	      </div>
-	    </div>
-	  </div>
 	</div>
 </div>
 @endsection
