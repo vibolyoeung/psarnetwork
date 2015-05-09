23d093ad6a4550c751f0b912b251a0dfdae9f5b8
Koch Doeun <kdoeun1@we-bridge.net>
2015-05-09 15:19:27 +0700

    addig progressing bar for loading success

--- public/frontend/js/popupdetails.js
+++ public/frontend/js/popupdetails.js
@@ -4,7 +4,9 @@ var popupDetails = (function(){
 	var add_popup_detail = function(product_id) {
 		var productUrl = document.querySelector('#detail_product');
   		productUrl = productUrl.dataset.getDetailProductUrl;
-		$('#details_view').load(productUrl + 'product/js_detail/' + product_id);
+  		$(".message-loading").show();
+		$('#details_view').load(productUrl + 'product/js_detail/' + product_id, function() {
+		});
 	};
 
 	return {
