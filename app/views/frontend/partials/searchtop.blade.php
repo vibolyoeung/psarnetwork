1c9fe54c9f8766a242db8cfb7f508f68bd6ecb80
kimhim <kimhim.hom@gmail.com>
2015-05-23 16:36:42 +0700

    add condition to Top Ads

--- app/views/frontend/partials/searchtop.blade.php
+++ app/views/frontend/partials/searchtop.blade.php
@@ -64,11 +64,19 @@
 						$exp_date = str_replace ( '/', '-', $exp_date );
 						if (strtotime ( date ( "d-m-Y" ) ) <= strtotime ( $exp_date )) {
 							?>
-						<a href="{{$adv->link_url}}"> <img
+						<a href="{{$adv->link_url}}">
+						<img
 							src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
 							class="img-responsive img-thumbanil" alt="" />
 						</a>
-						<?php } ?>
+						<?php }else{ ?>
+						<a href="#"> 
+							<img
+								src="{{Config::get('app.url')}}frontend/images/topspecialads.png"
+								class="img-responsive img-thumbnail" alt="" />
+						</a>
+						<?php 
+						} ?>
 					@endforeach
 						
 					<?php
