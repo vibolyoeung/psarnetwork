3c87a0378f636a13babe8f16d2a5761ca4ac82ab
Vibol Yoeung <yoeung.vibol@gmail.com>
2015-12-04 21:27:00 +0700

    Padding for advertisement

--- app/views/frontend/partials/horizontal_center_adv.blade.php
+++ app/views/frontend/partials/horizontal_center_adv.blade.php
@@ -5,10 +5,10 @@
 			$exp_date = $adv->end_date;
 			$exp_date = str_replace ( '/', '-', $exp_date );
 			if (strtotime ( date ( "d-m-Y" ) ) <= strtotime ( $exp_date )) { ?>
-					<a href="{{$adv->link_url}}"> <img
-				src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
-				class="img-responsive" alt="" />
-			</a>
+				<a href="{{$adv->link_url}}"> <img
+					src="{{Config::get('app.url')}}/upload/advertisement/{{$adv->image;}}"
+					class="img-responsive img-thumbnail" alt="" />
+				</a>
 			<?php } else { ?>
 				<img
 					src="{{Config::get('app.url')}}frontend/images/horizontal_default_advertise.png"
