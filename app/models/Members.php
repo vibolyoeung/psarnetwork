05e3c373daa9649e2571c2e77e539d6ca4cb15a7
Doeun KOCH <doeunkoch@gmail.com>
2015-12-30 22:40:39 +0700

    add file login

--- app/models/Members.php
+++ app/models/Members.php
@@ -14,14 +14,12 @@ class Members extends Eloquent{
 	 */
 	public function memberLogin($loginName, $password){
 		$query = DB::table(Config::get('constants.TABLE_NAME.USER'));
-		switch ($loginName){
-			case filter_var($loginName, FILTER_VALIDATE_EMAIL):
-				$query->where('email','=', $loginName);
-				break;
-			case is_numeric($loginName):
-				$query->where('telephone','=', $loginName);
-				break;
+		if (filter_var($loginName, FILTER_VALIDATE_EMAIL)) {
+			$query->where('email','=', $loginName);
+		} else {
+			$query->where('telephone','=', $loginName);
 		}
+		
 		$query->where('password', '=', md5(sha1($password)));
 		$query->where('user_type', '=', Config::get('constants.CLIENT_USER'));
 		$query->where('status', '=', self::STATUS);
