fe765e0a042d9db74d11551fa06717c65076d35b
kimhim <kimhim.hom@gmail.com>
2016-03-05 09:17:54 +0700

    more css

--- app/views/frontend/partials/details/right.blade.php
+++ app/views/frontend/partials/details/right.blade.php
@@ -1,5 +1,5 @@
 <!-- ========Right side========= -->
-<div class="right-sidebar">
+<div class="right-sidebar hidden-xs">
 	<!-- type:details, position: right meduim, limit -->
 		{{ App::make('FePageController')->getFeAds(3, 7, 3) }}
 	<!--=========Register seller============ -->
