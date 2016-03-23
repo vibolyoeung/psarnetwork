c5c1fff8e59446c468c5154e65623692f2a172ea
kimhim <kimhim.hom@gmail.com>
2016-03-23 19:45:02 +0700

    fixed static page

--- app/views/frontend/partials/static_page.blade.php
+++ app/views/frontend/partials/static_page.blade.php
@@ -1,5 +1,4 @@
-@extends('frontend.layout') 
-
+@extends('frontend.layout')
 @section('title') 
 	<?php  
 		echo $page->{"title_".App::getLocale()}
@@ -7,14 +6,6 @@
 @endsection
 
 @section('content')
-	<ol class="breadcrumb">
-		<li><a href="{{Config::get('app.url')}}">Home</a></li>
-		<li>
-			<?php  
-				echo $page->{"title_".App::getLocale()}
-			?> 
-		</li>
-	</ol>
 	<div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1" style='margin-top:5%;margin-bottom:3%;padding-bottom:10px;font-family:Hanuman !important;line-height:2;word-break:break-all;-webkit-box-shadow: -1px 5px 11px 9px rgba(227,227,227,0.5);-moz-box-shadow: -1px 5px 11px 9px rgba(227,227,227,0.5);box-shadow: -1px 5px 11px 9px rgba(227,227,227,0.5);'>
 		<h3>
 			<b>
